72d076ec652444935bd2f5fe1d208a0b3603a74e
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2024-11-25 18:15:20 -0300

    Agregando vista de productos

--- src/main/java/edu/unam/integrador/controllers/ProductoController.java
+++ src/main/java/edu/unam/integrador/controllers/ProductoController.java
@@ -1,11 +1,16 @@
 package edu.unam.integrador.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    @GetMapping("")
+    public String show(){
+        return "productos/show";
+    }
     
 }
