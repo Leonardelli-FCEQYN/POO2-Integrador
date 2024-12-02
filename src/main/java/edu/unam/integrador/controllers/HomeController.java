575ac51d3274f503cd8f88c16098279d2813d343
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2024-12-02 17:39:22 -0300

    Implementando parcialmente el carrito de compras

--- src/main/java/edu/unam/integrador/controllers/HomeController.java
+++ src/main/java/edu/unam/integrador/controllers/HomeController.java
@@ -1,12 +1,16 @@
 package edu.unam.integrador.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import edu.unam.integrador.model.Producto;
 import edu.unam.integrador.services.ProductoService;
 
 
@@ -24,8 +28,18 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Long id){
+    public String productoHome(@PathVariable Long id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.getProducto(id);
+        producto = optionalProducto.get();
+
+        model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(){
+        return "usuario/carrito";
     }
 
 }
